d82fccdf91ad1a36954f5ae4dbe7297c1bdee595
Sibi Daniel <sibidaniel95@gmail.com>
2018-08-19 21:06:59 +0530

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("aloha");
         String str = "LeinaD ibis";
-        Bus bus = new Bus("Sibi",3);
-        System.out.println(bus);
+       // Bus bus = new Bus("Sibi",3);
+        //System.out.println(bus);
         System.out.println("Happy coding");
          for(int i=0;i<str.length();i++){
              System.out.print(str.charAt(str.length()-i-1));
